bb74d3711d138973aec0320efe21640bc1cd570c
skyisthelimpet <108994454+skyisthelimpet@users.noreply.github.com>
2023-05-11 17:50:09 +0300

    Add files via upload

---
+++ build/crew.js
@@ -0,0 +1,46 @@
+
+function openNav() {
+    document.getElementById("sidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+}
+
+
+
+const buttonList = ['Douglas Hurley', 'Mark Shuttle', 'Victor Glover', 'Anous Ansari']
+
+const crewDiv = document.getElementById('crew-div');
+crewDiv.addEventListener('click', async function (event) {
+    const isButton = event.target.nodeName === 'BUTTON'
+    if (!isButton) {
+        return;
+    }
+    var index = event.target.id;
+
+    event.target.classList.add('actve')
+    for (let x = 0; x < buttonList.length; x++) {
+        const element = buttonList[x];
+        if (element != event.target.id) {
+            document.getElementById(element).classList.remove('actve')
+        }
+    }
+
+    const response = await fetch("/data.json");
+    const jsonData = await response.json();
+
+    const destList = {
+        'Douglas Hurley': 0,
+        'Mark Shuttle': 1,
+        'Victor Glover': 2,
+        'Anous Ansari': 3
+    }
+
+    document.getElementById('name').innerText = jsonData.crew[destList[index]].name.toUpperCase();
+    document.getElementById('role').innerText = jsonData.crew[destList[index]].role.toUpperCase();
+    document.getElementById('bio').innerText = jsonData.crew[destList[index]].bio;
+    document.getElementById('crew-desktop-photo').src = jsonData.crew[destList[index]].images.png
+    document.getElementById('crew-mobile-photo').src = jsonData.crew[destList[index]].images.png;
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+})
---
+++ build/script.js
@@ -0,0 +1,53 @@
+
+function openNav() {
+    document.getElementById("sidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+}
+
+const buttonList = ['Moon', 'Mars', 'Europa', 'Titan']
+
+try {
+    const destDiv = document.getElementById('dest-div');
+    destDiv.addEventListener('click', async function (event) {
+
+        const isButton = event.target.nodeName === 'BUTTON';
+        if (!isButton) {
+            return
+        }
+
+        var index = event.target.id;
+
+        const response = await fetch("/data.json");
+        const jsonData = await response.json();
+
+        const destList = {
+            'Moon': 0,
+            'Mars': 1,
+            'Europa': 2,
+            'Titan': 3
+        }
+
+        event.target.classList.add('border-b-2')
+        for (let x = 0; x < buttonList.length; x++) {
+            const element = buttonList[x];
+            if (element != event.target.id) {
+                document.getElementById(element).classList.remove('border-b-2')
+            }
+        }
+
+        document.getElementById('planet-photo').src = jsonData.destinations[destList[index]].images.png;
+        document.getElementById('destination-header').innerText = jsonData.destinations[destList[index]].name.toUpperCase()
+        document.getElementById('dest-paragraph').innerText = jsonData.destinations[destList[index]].description
+        document.getElementById('distance').innerText = jsonData.destinations[destList[index]].distance.toUpperCase()
+        document.getElementById('travel').innerText = jsonData.destinations[destList[index]].travel.toUpperCase()
+        window.scrollTo({ top: 0, behavior: 'smooth' })
+
+    })
+} catch (error) {
+    console.log('')
+}
+
+
